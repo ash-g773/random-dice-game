a17acb42f3bc974e2b694f3dde58e464eda343b0
ash-g773 <aishwaryageorge@ymail.com>
2026-05-01 12:15:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Roll the Dice - Hackathon Project!
+Welcome to our roll the dice game application! 
+- user inputs a guess first and then can click the dice
+    - html submit form
+- homepage - with dice, click it and it rolls
+    - animation from where?
+- announces a number
+- if they guess right confetti / winning animation
+- counter for correct vs incorrect guesses
+
+## Installation and Usage
+- html, css, javascript
+    - netlify
+
+## Technologies
+- html, css, javascript
+
+## How we made the game
+- how we split the work
+- git branches etc
+- developing additional features
+
+## Screenshots/Images
+
+## Wins & Challenges
+- everyone contributing to repo + utilizing branches
+- practicing pseudocode made logic much easier to translate into actual code
+
+
+Could have:
+    Badges
+    Contribution guide
+    Code snippets
+    Bugs
+    Future features
+    - increase number of dice
+    - send alert message as if the page is crashing / the computer is on to you
---
+++ index.js
@@ -0,0 +1,59 @@
+const guessForm = document.querySelector("#form")
+const diceRollImg = document.querySelector("#dice")
+
+guessForm.addEventListener("submit", getGuess)
+
+function getGuess(e) {
+    e.preventDefault() // Prevents automatic refresh when event occurs
+    console.log(validGuess(e.target[0].value)) // When the form is submitted, take what the user typed and give it to the validGuess function
+    e.target[0].value = "" // Clears the input field after submission
+}
+
+let countWins = 0
+let countLoss = 0
+
+function rollTheDice() {
+    // need to be between 1 and 6
+    let ans = Math.floor(Math.random() * 7)
+    while (ans === 0) {
+        ans = Math.floor(Math.random() * 7)
+    }
+    return ans
+}
+
+// check that the input is valid - no letters and between 1 and 6-
+function validGuess(guess) {
+    if (guess >= 7) {
+        alert("Invalid input, must be a number between 1 & 6")
+        // throw new Error("Invalid input, must be a number between 1 & 6")
+    } else if (guess <= 0) {
+        alert("Invalid input, must be a number between 1 & 6")
+    } else {
+        return guessMatch(guess)
+    }
+}
+
+function guessMatch(guess) {
+    const randomNumber = rollTheDice()
+    if (guess == randomNumber) {
+        countWins++
+        return `${guess} was right, well done!`
+    } else {
+        countLoss++
+        return `${guess} was wrong - better luck next time!`
+    }
+}
+
+
+/* BEGIN
+SET ans to random number using Math.floor and Math.random
+WHILE ans is 0, keep re-randomizing and assigning
+RETURN ans when its not 0
+END
+*/
+
+
+/* guessNumberMatch(guess) function
+BEGIN
+INPUT guess
+*/
